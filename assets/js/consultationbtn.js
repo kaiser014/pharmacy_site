c542752bb84293308b838d6a2592e5ba1dd12303
unknown <abukaiser.yuma>
2023-09-12 13:10:22 +0600

    add All files

---
+++ assets/js/consultationbtn.js
@@ -0,0 +1,196 @@
+function radiobtnyes1() {
+  var active = document.getElementById("aboutyes0001");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("aboutno0001");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status1").style.display = "none";
+}
+function radiobtnno1() {
+  var active = document.getElementById("aboutyes0001");
+  var inactive = document.getElementById("aboutno0001");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status1").style.display = "block";
+}
+function radiobtnyes2() {
+  var active = document.getElementById("aboutyes0002");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("aboutno0002");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status2").style.display = "block";
+}
+function radiobtnno2() {
+  var active = document.getElementById("aboutyes0002");
+  var inactive = document.getElementById("aboutno0002");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status2").style.display = "none";
+}
+function radiobtnyes3() {
+  var active = document.getElementById("aboutyes0003");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("aboutno0003");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status3").style.display = "none";
+}
+function radiobtnno3() {
+  var active = document.getElementById("aboutyes0003");
+  var inactive = document.getElementById("aboutno0003");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status3").style.display = "block";
+}
+function radiobtnyes4() {
+  var active = document.getElementById("symptomsyes0001");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("symptomsno0001");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status4").style.display = "none";
+}
+function radiobtnno4() {
+  var active = document.getElementById("symptomsyes0001");
+  var inactive = document.getElementById("symptomsno0001");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status4").style.display = "block";
+}
+function radiobtnyes5() {
+  var active = document.getElementById("healthyes0001");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0001");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status5").style.display = "block";
+}
+function radiobtnno5() {
+  var active = document.getElementById("healthyes0001");
+  var inactive = document.getElementById("healthno0001");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status5").style.display = "none";
+}
+function radiobtnyes6() {
+  var active = document.getElementById("healthyes0002");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0002");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status6").style.display = "block";
+}
+function radiobtnno6() {
+  var active = document.getElementById("healthyes0002");
+  var inactive = document.getElementById("healthno0002");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status6").style.display = "none";
+}
+function radiobtnyes7() {
+  var active = document.getElementById("healthyes0003");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0003");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status7").style.display = "block";
+}
+function radiobtnno7() {
+  var active = document.getElementById("healthyes0003");
+  var inactive = document.getElementById("healthno0003");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status7").style.display = "none";
+}
+function radiobtnyes8() {
+  var active = document.getElementById("healthyes0004");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0004");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status8").style.display = "block";
+}
+function radiobtnno8() {
+  var active = document.getElementById("healthyes0004");
+  var inactive = document.getElementById("healthno0004");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status8").style.display = "none";
+}
+function radiobtnyes9() {
+  var active = document.getElementById("healthyes0005");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0005");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status9").style.display = "block";
+}
+function radiobtnno9() {
+  var active = document.getElementById("healthyes0005");
+  var inactive = document.getElementById("healthno0005");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status9").style.display = "none";
+}
+function radiobtnyes10() {
+  var active = document.getElementById("healthyes0006");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0006");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status10").style.display = "block";
+}
+function radiobtnno10() {
+  var active = document.getElementById("healthyes0006");
+  var inactive = document.getElementById("healthno0006");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status10").style.display = "none";
+}
+function radiobtnyes11() {
+  var active = document.getElementById("healthyes0007");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("healthno0007");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status11").style.display = "block";
+}
+function radiobtnno11() {
+  var active = document.getElementById("healthyes0007");
+  var inactive = document.getElementById("healthno0007");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status11").style.display = "none";
+}
+function radiobtnyes12() {
+  var active = document.getElementById("agreementyes0001");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("agreementno0001");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status12").style.display = "none";
+}
+function radiobtnno12() {
+  var active = document.getElementById("agreementyes0001");
+  var inactive = document.getElementById("agreementno0001");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status12").style.display = "block";
+}
+function radiobtnyes13() {
+  var active = document.getElementById("agreementyes0002");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("agreementno0002");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status13").style.display = "none";
+}
+function radiobtnno13() {
+  var active = document.getElementById("agreementyes0002");
+  var inactive = document.getElementById("agreementno0002");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status13").style.display = "block";
+}
+function radiobtnyes14() {
+  var active = document.getElementById("agreementyes0003");
+  active.classList.add("radio-btn-set-active");
+  var inactive = document.getElementById("agreementno0003");
+  inactive.classList.remove("radio-btn-set-active");
+  document.getElementById("status14").style.display = "none";
+}
+function radiobtnno14() {
+  var active = document.getElementById("agreementyes0003");
+  var inactive = document.getElementById("agreementno0003");
+  active.classList.remove("radio-btn-set-active");
+  inactive.classList.add("radio-btn-set-active");
+  document.getElementById("status14").style.display = "block";
+}
